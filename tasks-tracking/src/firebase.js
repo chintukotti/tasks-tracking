be017086019983e11df3bbfe57888634169d5779
Satya <chintukotti78@gmail.com>
2025-12-30 15:16:09 +0530

    Remove Firebase configuration from firebase.js
    Removed sensitive Firebase configuration details from firebase.js.

--- tasks-tracking/src/firebase.js
+++ tasks-tracking/src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCCEdZRvQ1YK_yY2XI-PLjHr4wD6aZohmU",
-  authDomain: "tasks-tracking-59990.firebaseapp.com",
-  projectId: "tasks-tracking-59990",
-  storageBucket: "tasks-tracking-59990.firebasestorage.app",
-  messagingSenderId: "172986260700",
-  appId: "1:172986260700:web:a9b3ffeed7c6569a762819",
-};
+// -- Firebase configuration here
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
